ad22bfaa96a965d9d4828b2a9625d3027c7e6a48
Silvana Camargo <silvanacamargorocha86@gmail.com>
2024-02-29 15:31:58 -0300

    create de shirt controller

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,2 +1,28 @@
 class ShirtsController < ApplicationController
+
+  def new
+    @shirt = Shirts.new
+  end
+
+  def create
+    shirt = Shirt.new(shirt_params)
+    shirt.save
+    redirect_to shirts_path
+  end
+
+  def edit
+    @shirt = Shirt.find(params[:id])
+  end
+
+  def update
+    @shirt = Shirt.find(params[:id])
+    @shirt.update(shirt_params)
+    redirect_to shirt_path(@shirt)
+  end
+
+  def destroy
+    @shirt = Shirt.find(params[:id])
+    @shirt.destroy
+    redirect_to shirts_path
+  end
 end
