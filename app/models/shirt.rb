626953e057df7c81a51b3ad57212c5ee95a577a4
Almir Dias <almir.maradei@gmail.com>
2024-02-29 10:45:32 -0300

    modelos e controllers

---
+++ app/models/shirt.rb
@@ -0,0 +1,8 @@
+class Shirt < ApplicationRecord
+  belongs_to :user
+
+  validates :size, presence: true
+  validates :price, presence: true
+  validates :gender, presence: true
+  validates :team, presence: true, uniqueness:  true
+end
