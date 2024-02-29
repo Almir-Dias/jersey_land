6c18c120268bc8b034334560177ff1f841d43e2e
Almir Dias <almir.maradei@gmail.com>
2024-02-29 17:31:59 -0300

    order index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "shirts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
   resources :shirts do
-    resources :orders, only: [:index, :create]
+    resources :orders, only: [:create]
   end
+
+  resources :orders, only: [:index]
   # Defines the root path route ("/")
   # root "posts#index"
 end
