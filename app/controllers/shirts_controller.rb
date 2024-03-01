434a53d833367d260c1cdf4806fd45d37632e72d
Almir Dias <almir.maradei@gmail.com>
2024-03-01 16:52:30 -0300

    index e um pouco do show

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,5 +1,5 @@
 class ShirtsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def new
     @shirt = Shirt.new
