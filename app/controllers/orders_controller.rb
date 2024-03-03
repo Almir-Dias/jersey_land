9b9641338ca4f991a63a9f665a5a7bad5476990c
Almir Dias <almir.maradei@gmail.com>
2024-03-03 20:40:14 -0300

    fiz alteraçoes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,18 @@ class OrdersController < ApplicationController
     @orders = current_user.orders.includes(:shirt)
   end
 
+  # def show
+  #   @order = Order.find(params[:id])
+  #   @shirt = @order.shirt 
+  # end
+
   def show
-    @order = Order.find(params[:shirt_id])
-    @shirt = @order.shirt 
+    @order = Order.find_by(id: params[:id])
   end
 
+  def edit
+    @shirt = Shirt.find(params[:id])
+  end
   
   def create
     order = Order.new(shirt_id: params[:shirt_id], user_id: current_user.id)
